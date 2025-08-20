96c7d037fb20ccaa3643a2a730dbfd90d7f4821c
rodrigoprogramacao <rodrigofloresdacosta97@gmail.com>
2025-08-20 20:35:44 -0300

    Update horadecodar.c

--- horadecodar.c
+++ horadecodar.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main (){
+  int idade, matricula;
+  float altura;
+  char nome[50];
+
+printf("Digite sua idade: \n");
+scanf("%d, &idade);
+
+printf(Digite sua altura: \n");
+scanf("%f", &float);
+  
+
+}
